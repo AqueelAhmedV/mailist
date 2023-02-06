0bd11b1f4113a16aeddc0d57dce898705c4b112a
AqueelAhmedV <aqueelvallanchira13@gmail.com>
2023-02-06 20:23:20 +0530

    changed cors header origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser')
 const app = express();
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://mailist-admin.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
